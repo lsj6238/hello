82ae05969132a4414d7bfecfbb91f79fc0ebe569
Administrator <5555@qq.com>
2020-03-11 09:49:26 +0800

    3

--- src/com/lsj/Hello.java
+++ src/com/lsj/Hello.java
@@ -2,6 +2,6 @@ package com.lsj;
 
 public class Hello {
  public static void main(String[] args) {
-         System.out.println("ggggggghhh");
+         System.out.println("ggggggghhhhhhhhhh");
 }
 }
